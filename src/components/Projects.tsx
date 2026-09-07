bfc948d2e34d9c8b184546b15d4a0474683cee24
Integration Hub <66514702+Integration-Hub-DE@users.noreply.github.com>
2026-09-07 21:13:07 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -19,7 +19,7 @@ export default function Projects() {
           </p>
         </div>
 
-        // <div className="grid md:grid-cols-2 lg:grid-cols-6 gap-6">
+         {/* <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6"> */}
         <div className="grid md:grid-cols-2 lg:grid-cols-1 gap-6">
           {projects.map((project) => (
             <div
